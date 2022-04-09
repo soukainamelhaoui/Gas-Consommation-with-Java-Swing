ae84e8a581e13865e6151e5a352e1d4352645d29
soukainamelhaoui <soukaina.melhaoui@etu.uae.ac.ma>
2022-04-09 00:24:09 +0000

    Chauffeur and Vehicule Forms

--- src/ma/fstt/model/Bus.java
+++ src/ma/fstt/model/Bus.java
@@ -1,17 +1,33 @@
 package ma.fstt.model;
 
 public class Bus extends Vehicule{
-    public Bus(int idBus, String numero, String model, int capaRes, double poids) {
+    private int nbrSiege;
+    public Bus(int idBus, String numero, String model, int capaRes, double poids, int nbrSiege) {
         super(idBus, numero, model, capaRes, poids);
+        this.nbrSiege = nbrSiege;
+    }
+
+    public Bus() {
+
+    }
+
+    public int getNbrSiege() {
+        return nbrSiege;
+    }
+
+    public void setNbrSiege(int nbrSiege) {
+        this.nbrSiege = nbrSiege;
     }
 
     @Override
     public String toString() {
         return "Bus{" +
-                "idVehicule=" + idVehicule +
+                "nbrSiege=" + nbrSiege +
+                ", idVehicule=" + idVehicule +
                 ", numero='" + numero + '\'' +
                 ", model='" + model + '\'' +
                 ", capaRes=" + capaRes +
+                ", nbrSiege=" + nbrSiege +
                 ", poids=" + poids +
                 '}';
     }
--- src/ma/fstt/model/Cammion.java
+++ src/ma/fstt/model/Cammion.java
@@ -1,17 +1,33 @@
 package ma.fstt.model;
 
 public class Cammion extends Vehicule{
-    public Cammion(int idCammion, String numero, String model, int capaRes, double poids) {
+    private int capacite;
+    public Cammion(int idCammion, String numero, String model, int capaRes, double poids, int capacite) {
         super(idCammion, numero, model, capaRes, poids);
+        this.capacite = capacite;
+    }
+
+    public Cammion() {
+
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
     }
 
     @Override
     public String toString() {
         return "Cammion{" +
-                "idVehicule=" + idVehicule +
+                "capacite=" + capacite +
+                ", idVehicule=" + idVehicule +
                 ", numero='" + numero + '\'' +
                 ", model='" + model + '\'' +
                 ", capaRes=" + capaRes +
+                ", nbrSiege=" + nbrSiege +
                 ", poids=" + poids +
                 '}';
     }
